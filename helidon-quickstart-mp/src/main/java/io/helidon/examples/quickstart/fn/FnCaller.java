29eca888aac99de8f4a7e382abc56b1724d62abf
Chad Arimura <carimura@gmail.com>
2019-03-29 10:18:53 -0700

    ugly but works maybe

--- helidon-quickstart-mp/src/main/java/io/helidon/examples/quickstart/fn/FnCaller.java
+++ helidon-quickstart-mp/src/main/java/io/helidon/examples/quickstart/fn/FnCaller.java
@@ -1,6 +1,7 @@
 package io.helidon.examples.quickstart.fn;
 
 import java.io.BufferedReader;
+import java.io.DataOutputStream;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
@@ -8,17 +9,38 @@ import java.net.URL;
 
 public class FnCaller {
 
-    public static void callFn(String trigger) throws IOException {
-        String fnEndpoint = System.getenv("FN_INVOKE_ENDPOINT");
+    public static void callFn(String trigger, String input) throws IOException {
+        // TODO: Add back the env var once I figure out how vscode handles them.
+        //String fnEndpoint = System.getenv("FN_INVOKE_ENDPOINT");
+
+        String fnEndpoint = "http://localhost:8080/t/"; 
         URL url = new URL(fnEndpoint + trigger);
         HttpURLConnection con = (HttpURLConnection) url.openConnection();
-        con.setRequestMethod("GET");
-        BufferedReader in = new BufferedReader(new InputStreamReader(con.getInputStream()));
-        String inputLine;
-        while ((inputLine = in.readLine()) != null) {
-            System.out.println(inputLine);
-        }
-        in.close();
+        con.setRequestMethod("POST");
+
+        // Send post request
+		con.setDoOutput(true);
+		DataOutputStream wr = new DataOutputStream(con.getOutputStream());
+		wr.writeBytes(input);
+		wr.flush();
+        wr.close();
+        
+        int responseCode = con.getResponseCode();
+		System.out.println("\nSending 'POST' request to URL : " + url);
+		System.out.println("Post parameters : " + input);
+		System.out.println("Response Code : " + responseCode);
+
+		BufferedReader in = new BufferedReader(
+		        new InputStreamReader(con.getInputStream()));
+		String inputLine;
+		StringBuffer response = new StringBuffer();
+
+		while ((inputLine = in.readLine()) != null) {
+			response.append(inputLine);
+		}
+		in.close();
+		
+		System.out.println(response.toString());
     }
 
 }
